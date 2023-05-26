cb0a14ed1bbfbe9007693028583a2d15e3302061
danielmegale <danim1@live.it>
2023-05-26 18:56:54 +0200

    fix negative value

--- script.js
+++ script.js
@@ -9,23 +9,24 @@ const Km =document.getElementById('user-km')
 //User Info
 const userAge =parseInt(prompt('Inserisci la tua età', 45));
 console.log(userAge + ' ' + 'Anni', typeof userAge);
-user.innerText +=' ' + userAge + 'Anni';
+
 
 const userKm =parseInt(prompt('Inerisci i kilometri che vuoi percorrere',100));
 console.log(userKm+'Km', typeof userKm);
-Km.innerText +=' '+ userKm + 'Km'
 
 //Validatore
 let coupon= 1;
 let newPrice;
 if (isNaN(userAge) || isNaN(userKm)){
     alert('Valore non valido')
-}
-//Ticket Price
-else{
+}else if ((userAge < 0) || (userKm < 0)){
+    alert('Valore negativo non valido')
+}else{
     const ticketPrice = (userKm * KmPrice).toFixed(2);
     console.log(ticketPrice);
     element.innerText +=' '+ ticketPrice +'€';
+    user.innerText +=' ' + userAge + 'Anni';
+    Km.innerText +=' '+ userKm + 'Km';
 
     if (userAge < 18 ){
         coupon = 0.80;
@@ -33,8 +34,7 @@ else{
         newPrice = ticketPrice * coupon;
         console.log(newPrice);
         couponTicket.innerText +='Biglietto scontato' + ' ' +  (newPrice).toFixed(2) + '€';
-    }
-    else if (userAge >=65){
+    }else if (userAge >=65){
         coupon = 0.60;
         console.log(coupon);
         newPrice = ticketPrice * coupon;
