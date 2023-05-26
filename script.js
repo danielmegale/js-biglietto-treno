22532bdc718739d710a36bc86c2cd1b456e3e97e
danielmegale <danim1@live.it>
2023-05-26 15:38:45 +0200

    added userinfo

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('JS OK')
+
+//Prendiamo un elemnto dal DOM
+const element = document.getElementById('ticket');
+
+//User Info
+const userAge = prompt('Inserisci la tua età', 45);
+console.log(userAge + ' ' + 'Eta');  
+const userKm = prompt('Inerisci i kilometri che vuoi percorrere',169);
+console.log(userKm+'Km')
+
